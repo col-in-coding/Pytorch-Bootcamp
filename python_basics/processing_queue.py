d7e28f05dc526654bc1cdb2ecb00229652f46f1b
lihaoxiang1989 <hxl.colin@gmail.com>
2020-07-09 22:13:49 +0800

    multiprocess queue is processing secure

--- python_basics/processing_queue.py
+++ python_basics/processing_queue.py
@@ -1,12 +1,21 @@
 import multiprocessing as mp
+import time
 
-def job(q, a, b):
-    q.put(a + b)
+def sub_task(cond, q):
+    name = mp.current_process().name
+    print("Starting: ", name)
+    while True:
+        i = q.get()
+        print("accept: ", i)
+
 
 if __name__ == "__main__":
+    cond = mp.Condition()
     q = mp.Queue()
-    p1 = mp.Process(target=job, args=(q, 1, 2))
+    p1 = mp.Process(name='p1', target=sub_task, args=(cond, q))
+    p1.daemon = True
     p1.start()
-    p1.join()
-    res = q.get()
-    print(res)
+
+    for i in range(10):
+        time.sleep(1)
+        q.put(i)
